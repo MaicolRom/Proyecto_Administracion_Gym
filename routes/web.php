19acdf5be4795d27c1c1c132037cdc3f60e77ce9
santiago434c <75103119+santiago434c@users.noreply.github.com>
2022-11-22 09:38:28 -0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::resource('afiliados',  App\Http\Controllers\AfiliadoController::class);
+Route::resource('sede',  App\Http\Controllers\SedeController::class);
+Route::resource('entrenadores',  App\Http\Controllers\EntrenadoreController::class);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
